99843be498fa7b6e8b2fb4ef31098a73ea02cb6a
Oscar Jums <oscarjuma2004dr@yahoo.com>
2018-09-23 03:57:57 -0500

    TriviaGame

---
+++ assests/javascript/app.js
@@ -0,0 +1,321 @@
+var triviaQuestions = [{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+	answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+},{
+	question: "Name the state! Everything is bigger",
+	answerChoices: ["California", "Texas", "Louisiana", "Mexico"],
+    answer: 1
+}];
+
+var mapArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10', 'question11', 'question12', 'question13','question14','question15','question16','question17','question18','question19','question20','question21','question22','question23','question24','question25','question26','question27','question28','question29','question30','question31','question32','question33','question34','question35','question36','question37','question38','question39','question40','question41','question42','question43','question44','question45','question46','question47','question48','question49','question50'];
+var currentQuestion; var correctAns; var incorrectAnswer; var noAnswer; var seconds; var time; var answered; var userSelect;
+var messages = {
+	correct: "Correct!",
+	incorrect: "Need a state by state tour?",
+	endTime: "Were you googling?",
+	finished: "How did you do?"
+}
+
+$('#startBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+$('#startOverBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+function newGame(){
+	$('#finalMessage').empty();
+	$('#correctAnswers').empty();
+	$('#incorrectAnswers').empty();
+	$('#noAnswer').empty();
+	currentQuestion = 0;
+	correctAns = 0;
+	incorrectAnswer = 0;
+	noAnswer = 0;
+	newQuestion();
+}
+
+function newQuestion(){
+	$('#message').empty();
+	$('#correctAns').empty();
+	$('#gif').empty();
+	answered = true;
+	
+	//sets up new questions & answerChoices
+	$('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+triviaQuestions.length);
+	$('.question').html('<h2>' + triviaQuestions[currentQuestion].question + '</h2>');
+	for(var i = 0; i < 4; i++){
+		var choices = $('<div>');
+		choices.text(triviaQuestions[currentQuestion].answerChoices[i]);
+		choices.attr({'data-index': i });
+		choices.addClass('thisChoice');
+		$('.answerChoices').append(choices);
+	}
+	countdown();
+	//clicking an answer will pause the time and setup answerPage
+	$('.thisChoice').on('click',function(){
+		userSelect = $(this).data('index');
+		clearInterval(time);
+		answerPage();
+	});
+}
+
+function countdown(){
+	seconds = 20;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	answered = true;
+	//sets timer to go down
+	time = setInterval(showCountdown, 1000);
+}
+
+function showCountdown(){
+	seconds--;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	if(seconds < 1){
+		clearInterval(time);
+		answered = false;
+		answerPage();
+	}
+}
+
+function answerPage(){
+	$('#currentQuestion').empty();
+	$('.thisChoice').empty(); //Clears question page
+	$('.question').empty();
+
+	var rightAnswerText = triviaQuestions[currentQuestion].answerChoices[triviaQuestions[currentQuestion].answer];
+	var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+	$('#img').html('<img src = "assets/images/'+ imgArray[currentQuestion] +'.img" width = "350px">');
+	//checks to see correct, incorrect, or unanswered
+	if((userSelect == rightAnswerIndex) && (answered == true)){
+		correctAnswers++;
+		$('#message').html(messages.correct);
+	} else if((userSelect != rightAnswerIndex) && (answered == true)){
+		incorrectAnswers++;
+		$('#message').html(messages.incorrect);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+	} else{
+		unanswered++;
+		$('#message').html(messages.endTime);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+		answered = true;
+	}
+	
+	if(currentQuestion == (triviaQuestions.length-1)){
+		setTimeout(scoreboard, 5000)
+	} else{
+		currentQuestion++;
+		setTimeout(newQuestion, 5000);
+	}	
+}
+
+function scoreboard(){
+	$('#timeLeft').empty();
+	$('#message').empty();
+	$('#correctAns').empty();
+	$('#gif').empty();
+
+	$('#finalMessage').html(messages.finished);
+	$('#correctAnswers').html("Correct Answers: " + correctAns);
+	$('#incorrectAnswers').html("Incorrect Answers: " + incorrectAns);
+	$('#noAnswer').html("No Answer: " + noAnswer);
+	$('#startOverBtn').addClass('reset');
+	$('#startOverBtn').show();
+	$('#startOverBtn').html('Start Over?');
+}
